f6f7915e1f2f36beb1ec5b8059b774cf695c1e73
Abdo Roig-Maranges <abdo.roig@gmail.com>
2013-06-22 14:20:55 +0200

    added check_paths function

--- async/hosts/base.py
+++ async/hosts/base.py
@@ -193,12 +193,21 @@ class BaseHost(object):
             ui.print_debug("path %s does not exist" % self.path)
             return False
 
-        # additional path checks
-        # TODO: sure I want this here?
+        return True
+
+
+    def check_paths(self):
+        """Checks directories that are required to be present. Returns true if all checks
+        pass, or raises an exception with information about what failed."""
+
+        if not check_devices():
+            raise HostException("There are unmounted devices")
+
         for p in self.check:
             path = os.path.join(self.path, p)
             if not self.path_exists(path):
                 ui.print_debug("path %s does nit exist" % path)
+                raise HostException("path %s does nit exist" % path)
                 return False
 
         return True
