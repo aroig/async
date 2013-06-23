73542ad4a707c64d8bf0ebaf2887cbcda1b9c67e
Abdo Roig-Maranges <abdo.roig@gmail.com>
2013-06-23 17:28:31 +0200

    better handling of interactive process (like unison)

--- async/cmd.py
+++ async/cmd.py
@@ -22,40 +22,116 @@ import sys
 import os
 import re
 
+from threading  import Thread
+from Queue import Queue, Empty
+
 import async.archui as ui
 
+
 def print_rsync_line(line):
-    ui.write_color("  %s" % line)
+    ui.write_color("%s" % line)
 
 
 def print_unison_line(line):
-    ui.write_color("  %s" % line)
+    ui.write_color("%s" % line)
 
 
 def print_annex_line(line):
-    ui.write_color("  %s" % line)
+    ui.write_color("%s" % line)
+
+
+class StdoutWriter(Thread):
+    def __init__(self, stream, char_callback, line_callback):
+        super(StdoutWriter, self).__init__()
+        self.stream = stream
+        self.char_callback = char_callback
+        self.line_callback = line_callback
+        self.force_newline = False
+        self.daemon = True
+
+    def run(self):
+        line0='  '
+
+        line = line0
+        b = 'a'
+        while len(b) > 0:
+            b = self.stream.read(1)
+
+            if self.force_newline: line = line + '\n'
+            else:                  line = line + b
+
+            # when forcing a newline, just reset line without calling callback. We can't
+            # guarantee an other '\n' (from stdin for example) messes up with the rewriting.
+            if self.force_newline:
+                # the character I read belongs to the next line!
+                line = line0 + b
+                self.force_newline = False
+
+                if self.char_callback: self.char_callback(line)
+
+            # reset line and call callback to rewrite the line. we know there is no extra '\n'
+            # in the tty.
+            elif b == '\n':
+                if self.char_callback: self.char_callback('\r')
+                if self.line_callback: self.line_callback(line)
+                line = line0
+                if self.char_callback: self.char_callback(line)
+
+            else:
+                if self.char_callback: self.char_callback(b)
+
+        self.stream.close()
+
+
+class StdinReader(Thread):
+    def __init__(self, stream, char_callback, line_callback):
+        super(StdinReader, self).__init__()
+        self.stream = stream
+        self.char_callback = char_callback
+        self.line_callback = line_callback
+        self.daemon = True
+
+    def run(self):
+        line = ''
+        b = 'a'
+        while len(b) > 0:
+            b = self.stream.read(1)
+            line = line + b
+            if b == '\n':
+                if self.line_callback: self.line_callback(line)
+                line = ''
+        self.stream.close()
 
 
 def run_stream(args, callback=None, cwd=None):
-    """Runs a process, streaming its stdout through a callback function"""
-    proc = subprocess.Popen(args, stderr=subprocess.STDOUT, stdout=subprocess.PIPE, cwd=cwd)
+    """Runs a process, streaming its stdout through a callback function, but still accepting
+       interactive input"""
 
-    # loop over output and detect lines ended with '\n'
-    b = 'a'
-    lineb = ''
-    stop = False
-    while len(b) > 0:
-        b = proc.stdout.read(1)
-        lineb = lineb + b
+    # process and queue
+    proc = subprocess.Popen(args, stderr=subprocess.STDOUT, stdout=subprocess.PIPE, stdin=subprocess.PIPE, cwd=cwd)
+    force_newline = False
 
-        # catch end of line
-        if b == '\n':
-            if callback: callback(lineb)
 
-            lineb = ''
-            if stop: break
+    def char_to_sys_stdout(b):
+        sys.stdout.write(b)
+        sys.stdout.flush()
 
+    stdout_writer = StdoutWriter(stream=proc.stdout, char_callback=char_to_sys_stdout, line_callback=callback)
+
+
+    def line_to_proc_stdin(line):
+        proc.stdin.write(line)
+        proc.stdin.flush()
+        stdout_writer.force_newline = True
+
+    stdin_reader = StdinReader(stream=sys.stdin, char_callback=None, line_callback=line_to_proc_stdin)
+
+    stdout_writer.start()
+    stdin_reader.start()
+
+    # wait until process finishes
     ret = proc.wait()
+
     if ret:
         raise subprocess.CalledProcessError(ret, ' '.join(args))
 
