f872a1686a4e2a77887ec2643e48ce6190e753a9
Abdo Roig-Maranges <abdo.roig@gmail.com>
2013-06-25 13:37:42 +0200

    fix missing exception

--- async/hosts/ec2.py
+++ async/hosts/ec2.py
@@ -18,6 +18,8 @@
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from async.hosts.ssh import SshHost
+from async.hosts.base import HostError
+
 from boto import ec2
 from boto.exception import EC2ResponseError
 
