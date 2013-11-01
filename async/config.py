87a12007441ee38b5c50c6f28658c2e0cb2535b2
Abdo Roig-Maranges <abdo.roig@gmail.com>
2013-11-01 22:10:41 +0100

    rewrite the config parsing
    Now I can have dictionary config parameters like
    uuid:lib=blah blah
    
    Also I can extend lists from their default value with
    my_list=,new,elements

--- async/config.py
+++ async/config.py
@@ -28,72 +28,129 @@ class AsyncConfigError(Exception):
         super(AsyncConfigError, self).__init__(msg)
 
 
-def parse_string(s):
-    if s == None: return None
-    return s.strip()
+# Parsers
+# --------------------------------------------------------------------
+# All these parsers accept ket, val, dic. The parse key, val and store it in dic.
+
+# key: key for the entry
+# val: value to be parsed. If none, ignore it silently
+# old: dictionary containing
+
+def parse_string(key, val, dic):
+    if val:
+        dic[key] = val.strip()
+    return dic.get(key, None)
 
 
-def parse_bool(s):
-    if s == None: return None
-    val = s.strip().lower()
-    if val in set(['on', 'true', '1', 'yes']): return True
-    elif val in set(['off', 'false', '0', 'no']): return False
-    else: raise ValueError("Unrecognized boolean value: %s" % s)
+
+def parse_bool(key, val, dic):
+    if val:
+        val = val.strip().lower()
+        bval = False
+
+        if val in set(['on', 'true', '1', 'yes']): bval = True
+        elif val in set(['off', 'false', '0', 'no']): bval = False
+        else: raise ValueError("Unrecognized boolean value: %s" % s)
+
+        dic[key] = bval
+    return dic.get(key, None)
 
 
-def parse_path(s):
-    if s == None: return None
-    return os.path.expandvars(os.path.expanduser(s.strip()))
+
+def parse_path(key, val, dic):
+    if val:
+        dic[key] = os.path.expandvars(os.path.expanduser(val.strip()))
+    return dic.get(key, None)
 
 
-def parse_list(s, parse_val=parse_string):
-    if s == None: return []
-    return [parse_val(it) for it in s.split(',')]
+
+def parse_list(key, val, dic, parse_val=parse_string):
+    if not key in dic: dic[key] = []
+
+    if val:
+        L = val.split(',')
+
+        # do not accumulate with previous values if the list starts with a comma.
+        if len(L[0].strip()) == 0: L=L[1:]
+        else:                      dic[key] = []
+
+        for it in L:
+            v = parse_val('key', it, {})
+            if v:
+                dic[key].append(v)
+    return dic[key]
 
 
-def parse_list_args(s):
+
+def parse_list_args(key, val, dic):
     # TODO: handle quoted strings
-    if s == None: return []
-    return [it.strip() for it in s.split(' ')]
+    if not key in dic: dic[key] = []
+
+    if val:
+        dic[key] = []                  # never accumulate args
+        for it in val.split(' '):
+            dic[key].append(it.strip())
+    return dic[key]
 
 
-def parse_dict(s, parse_val=parse_string):
-    if s == None: return {}
-    dic = {}
 
-    for it in s.split(','):
-        spl = it.strip().split(':')
-        if len(spl) > 2:
-            raise AsyncConfigError("Can't parse keyval string: %s" % s)
-            break
-        elif len(spl) == 0:
-            continue
+def parse_dict(key, val, dic, parse_val=parse_string):
+    """ex. dict=A:1,B:2,C:3"""
+    if not key in dic: dic[key] = {}
 
-        if len(spl) == 2:  val=spl[1].strip()
-        else:              val=spl[0].strip()
+    if val:
+        L = val.split(',')
 
-        if parse_val:      val = parse_val(val)
+        # do not accumulate with previous values if the list starts with a comma.
+        if len(L[0].strip()) == 0: L=L[1:]
+        else:                      dic[key] = {}
 
-        if len(spl) == 2:  key=spl[0].strip()
-        else:              key=val
+        for it in L:
+            spl = it.strip().split(':', 1)
+            if len(spl) == 0:
+                continue
 
-        dic[key] = val
+            elif len(spl) == 2:
+                parse_val(spl[0].strip(), spl[1].strip(), dic[key])
 
-    return dic
+            else:
+                raise ValueError("Wrong value for dict field: %s" % key)
+    return dic[key]
 
 
-def parse_dict_path(s):
-    return parse_dict(s, parse_path)
 
-def parse_list_path(s):
-    return parse_list(s, parse_path)
+def parse_keyval(key, val, dic, parse_val=parse_string):
+    """ex. uuid:directory = blahblah"""
+    ret = {}
+    spl = key.strip().split('.', 1)
+    if len(spl) == 0:
+        return
+
+    elif len(spl) == 2:
+        key = spl[0]
+        nk = spl[1]
+
+    else:
+        raise ValueError("Wrong key for a keyval field: %s" % key)
+
+    if not key in dic: dic[key] = {}
+    if val: parse_val(nk, val, dic[key])
+
+
+
+def parse_dict_path(key, val, dic):
+    return parse_dict(key, val, dic, parse_path)
+
+
+
+def parse_list_path(key, val, dic):
+    return parse_list(key, val, dic, parse_path)
 
 
 
 class AsyncConfig(ConfigParser):
 
     # default values and parsing functions
-
     HOST_FIELDS={
         'dirs'           : ([], parse_list),
         'ignore'         : ([], parse_list_path),
@@ -140,6 +197,7 @@ class AsyncConfig(ConfigParser):
     REMOTE_FIELDS={
         'url'            : (None, parse_string),
         'git_hooks'      : ({}, parse_dict_path),
+        'uuid\..*'       : ({}, parse_keyval),
     }
 
     DIRECTORY_FIELDS={
@@ -168,15 +226,24 @@ class AsyncConfig(ConfigParser):
 
     def _parse_config(self, sec, fields, defaults):
         dic = {'conf_path': self.path}
-        for k, pair in fields.items():
-            func = pair[1]
+        for key, pair in fields.items():
+            func   = pair[1]
+            initval = pair[0]
 
-            if self.has_option(sec, k): val = self.get(sec, k)
-            else:                       val = defaults.get(k, None)
+            # if key contains a star, it is a regexp. match it!
+            if '*' in key:
+                L = [k for k, v in self.items(sec) if re.match(key, k)]
+            else:
+                L = [key]
+                if not key in dic: dic[key] = initval
 
-            if val:  dic[k] = func(val)
-            else:    dic[k] = pair[0]
+            for key in L:
+                if self.has_option(sec, key): val = self.get(sec, key)
+                else:                         val = None
 
+                defval = defaults.get(key, None)
+                func(key, defval, dic)                 # parse the default value
+                func(key, val, dic)                    # parse the value
         return dic
 
 
