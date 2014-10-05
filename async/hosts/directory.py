5deeabc27735643e3456b4c31b45c1b0d71af8c4
Abdo Roig-Maranges <abdo.roig@gmail.com>
2014-10-05 20:19:22 +0200

    fix python 3 encoding thing

--- async/hosts/directory.py
+++ async/hosts/directory.py
@@ -116,7 +116,7 @@ class DirectoryHost(BaseHost):
         stdout, stderr = proc.communicate(stdin)
 
         if proc.returncode != 0:
-            raise CmdError("Local command failed", cm, proc.returncode, stdout or "")
+            raise CmdError("Local command failed", cm, proc.returncode, stdout.decode() or "")
 
         if catchout: return stdout.decode()
         else:        return None
